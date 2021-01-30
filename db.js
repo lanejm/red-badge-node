07911dc98868397bba1bc1796eec3a0776263277
Claire Lane <clairelane@Claires-MacBook-Air-2.local>
2021-01-30 13:52:43 -0500

    heroku

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 
-const database = new Sequelize(process.env.DATABASE_URL, 'postgres', process.env.PASS, {
+const database = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
