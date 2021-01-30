1bb83ceeb79bf6bd63d6a5aec6c82aa6ec97617d
Claire Lane <clairelane@Claires-MacBook-Air-2.local>
2021-01-30 14:12:10 -0500

    heroku

--- db.js
+++ db.js
@@ -1,9 +1,19 @@
 const Sequelize = require('sequelize');
 
 
+// const database = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: 'postgres'
+// });
+
 const database = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres'
-});
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION 
+      },
+    },
+  });
 
 database.authenticate()
 .then(() => console.log('postgres database is connected'))
